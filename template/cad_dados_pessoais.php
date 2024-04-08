1668be83552813071b92f88ac209549420dc01c0
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2024-04-08 15:54:44 -0300

    inicial
    inicial

---
+++ template/cad_dados_pessoais.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<?php include "head.php"; ?>
+
+<body class="">
+  <div class="wrapper ">
+    <?php include "menu.php"; ?>
+    <div class="main-panel">
+      <!-- Navbar -->
+      <?php include "nav_superior.php"; ?>
+      <!-- End Navbar -->
+      <div class="content">
+        <!--cards-->
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card ">
+              <div class="card-header ">
+                <h5 class="card-title">Cadastro de Funcionários</h5>
+                <p class="card-category">Preencha os campos</p>
+              </div>
+              <div class="card-body ">
+                
+              </div>
+              <div class="card-footer ">
+                <hr>
+                <div class="stats">
+                  <i class="fa fa-history"></i> texto
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php include "footer.php"; ?>
+    </div>
+  </div>
+  <?php include "js.php"; ?>
+ 
+</body>
+
+</html>
