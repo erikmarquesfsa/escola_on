f2d843aace19498927bc5fa369a166ad0fc3aaff
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2024-04-15 19:08:23 -0300

    .
    .

--- template/head.php
+++ template/head.php
@@ -1,10 +1,19 @@
+<?php
+session_start();
+$login = $_SESSION['login'];
+if(empty($_SESSION['login'])){
+  echo "<script>alert('Ops! Logue novamente!');</script>";
+  echo "<script>window.location='../index.php'</script>";
+}
+include "../_scripts/functions.php";
+?>
 <head>
   <meta charset="utf-8" />
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
-    Paper Dashboard 2 by Creative Tim
+    Escola On
   </title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
