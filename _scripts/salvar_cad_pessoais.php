69bafce346543471bbe0ff467a353efe421c3547
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2024-04-26 21:15:32 -0300

    .
    .

--- _scripts/salvar_cad_pessoais.php
+++ _scripts/salvar_cad_pessoais.php
@@ -1,3 +1,8 @@
+<html>
+<head>
+    <body>
+        <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <?php
 include "config.php";
 
@@ -9,20 +14,24 @@ $bairro = $_POST['bairro'];
 $cidade = $_POST['cidade'];
 $funcao = $_POST['funcao'];
 
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
 
 $sql = "INSERT INTO cad_pessoais (nome,email,cep,endereco,bairro,cidade,funcao) VALUES ('$nome','$email','$cep','$endereco','$bairro','$cidade','$funcao')";
 $query = $mysqli->query($sql);
 
-if($query){
-    echo "Rodou lindo!";
-}else{
-    echo "Deu ruim!";
+if($query){?>
+    <script>
+                Swal.fire({
+                    title: "Sucesso!",
+                    text: "Cadastro Realizado com Sucesso.",
+                    icon: "success"
+                    });
+            </script>
+                
+<?php }else{
+    echo "<script>alert('Login inválido');</script>";
+    echo "<script>window.location='../index.php'</script>";
 }
 
-
-
-
 ?>
+    </body>
+</html>
